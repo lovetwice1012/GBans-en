f5b9b6de35c59e908eeb5c10c3f724dbf33e437b
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-09-28 08:11:25 +0900

    Update Main.php

--- src/lovetwice1012/gbans/Main.php
+++ src/lovetwice1012/gbans/Main.php
@@ -37,37 +37,6 @@ class Main extends PluginBase implements Listener
         	$this->config = new Config($this->getDataFolder() . "whitelist.yml", Config::YAML);
 		$this->config2 = new Config($this->getDataFolder() . "cip.yml", Config::YAML);
         	$this->config3 = new Config($this->getDataFolder() . "uid.yml", Config::YAML);
-        	$this->config4 = new Config($this->getDataFolder() . "licensekey.yml", Config::YAML);
-	    	$url = 'http://passionalldb.s1008.xrea.com/gban/ver2.php';
-
-        	$data = array(
-            	'checkver' => 'checkver'
-        	);
-
-        	$context = array(
-            		'http' => array(
-                		'method'  => 'POST',
-                		'header'  => implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',)),
-                		'content' => http_build_query($data)
-            		)
-        	);
-	
-		$result = @file_get_contents($url, false, stream_context_create($context));
-	   	$data=json_decode($result);
-	   	$next = $data[0];
-	    	$VI = $data[1];
-	   	if($this->cver!=$next){
-			if($VI){
-				$this->alert=true;   
-			    	$this->getLogger()->info(Color::RED . "[GBan]とても重要なアップデートがあります。アップデートしないと、GBanの動作に致命的な影響を及ぼす可能性があります。すぐにアップデートをしてください。");
-		    	}else{
-			    	$this->alert=false;  
-			    	$this->getLogger()->info(Color::RED . "[GBan]新しいバージョンがあります。アップデートしてください。");
-		    	}
-	    	}else{
-			$this->alert=false;      
-	    	}
-	    	
     }
     public function onPreLogin(PlayerPreLoginEvent $event){
 	$player = $event->getPlayer();
@@ -76,7 +45,7 @@ class Main extends PluginBase implements Listener
     	$uid = (string)$player->getUniqueId();
 	if(!$this->config->exists($name)){
         	if($this->isbanned($name,$cip,(string)$uid)){
-        		$event->setkickMessage("§4あなたはBANされています。");
+        		$event->setkickMessage("§4You are banned.");
         		$event->setCancelled();
         	}
 	}
@@ -90,36 +59,6 @@ class Main extends PluginBase implements Listener
             $this->config3->set($name,$uid);
 	    $this->config2->save();
             $this->config3->save();
-	    $url = 'http://passionalldb.s1008.xrea.com/gban/ver2.php';
-	    $data = array(
-            	'checkver' => 'checkver'
-	    );
-	    $context = array(
-            	'http' => array(
-                	'method'  => 'POST',
-                	'header'  => implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',)),
-                	'content' => http_build_query($data)
-            	)
-	    );
-
-            $result = @file_get_contents($url, false, stream_context_create($context));
-	    $data=json_decode($result);
-	    $next = $data[0];
-	    $VI = $data[1];
-	    if($this->cver!=$next){
-		    if($VI){
-		            $this->alert=true;   
-			    $this->getLogger()->info(Color::RED . "[GBan]とても重要なアップデートがあります。アップデートしないと、GBanの動作に致命的な影響を及ぼす可能性があります。すぐにアップデートをしてください。");
-		    }else{
-			    $this->alert=false;  
-			    $this->getLogger()->info(Color::RED . "[GBan]新しいバージョンがあります。アップデートしてください。");
-		    }
-	    }else{
-		$this->alert=false;      
-	    }
-            if($this->alert&&$event->getPlayer()->isOp()){
-		$event->getPlayer()->sendMessage("§4[GBan]とても重要なアップデートがあります。アップデートしないと、GBanの動作に致命的な影響を及ぼす可能性があります。すぐにアップデートをしてください。");
-	    }
 	}
     public function isbanned($name,$ip,$uid){
 
@@ -151,17 +90,17 @@ class Main extends PluginBase implements Listener
     public function onCommand(CommandSender $sender, Command $command, string $label, array $args):bool
 	{
 	    if (!$sender instanceof Player){
-		    $this->getLogger()->info(Color::RED . "コンソールからの操作はサポート外になりました。");
+		    $this->getLogger()->info(Color::RED . "Operations from the console are no longer supported.");
 	            return true;
 	    }
 	    if ($command->getName() === "gban"){
             
             	if (empty($args[0])||empty($args[1])){
-                	$sender->sendMessage(" §b使い方 : /gban <プレイヤーのゲーマータグ> <理由>");
+                	$sender->sendMessage(" §bHow to use: / gban <player gamertag> <reason>");
                 	return true;
             	}
             	if (!$this->config2->exists($args[0])||!$this->config3->exists($args[0])){
-			$sender->sendMessage(" §4サーバーに来たことのないユーザーはGBanできません。");
+			$sender->sendMessage(" §4Users who have never been to the server cannot GBan.");
                 	return true;    
 	    	}
             	if($this->ban($args[0],$args[1],$sender->getName(),$this->config2->get($args[0]),(string)$this->config3->get($args[0]))){
@@ -169,10 +108,10 @@ class Main extends PluginBase implements Listener
                         if ($player instanceof Player){
 			    $player->setBanned(true);
 	                }	    
-		  	$sender->sendMessage("グローバルbanしました。response: \"".$this->message."\"");  
+		  	$sender->sendMessage("Global ban. response: \"".$this->message."\"");  
                	 	return true;
            	}else{
-			$sender->sendMessage("グローバルbanできませんでした。しばらく後に再実行して下さい。 response: \"".$this->message."\"");
+			$sender->sendMessage("Global ban could not be done.  Please try again after a while. response: \"".$this->message."\"");
                 	return true;
             	}
 	     
@@ -180,15 +119,15 @@ class Main extends PluginBase implements Listener
             if ($command->getName() === "gunban"){
             
            	if (empty($args[0])){
-               	 	$sender->sendMessage(" §b使い方 : /gunban <プレイヤーのゲーマータグ>");
+               	 	$sender->sendMessage(" §bHou to use : /gunban gamertag");
                 	return true;
             	}
             	if($this->unban($args[0],$sender->getName())){
-           		$sender->sendMessage("このサーバーからの被害の報告を取り消しました。 response: \"".$this->message."\"");
+           		$sender->sendMessage("Canceled the damage report from this server. response: \"".$this->message."\"");
                 	return true;
             	}else{
-		     	$sender->sendMessage("このサーバーからの被害の報告を取り消すことができませんでした。リクエストが拒否されました。 response: \"".$this->message."\"");
-                    	$sender->sendMessage("§4[注意]UNBANコマンドはその人をBANしたOP本人が実行していて、その人をBANした時にいたサーバーで行わないと拒否されます。");
+		     	$sender->sendMessage("The damage report from this server could not be undone.  The request has been rejected. response: \"".$this->message."\"");
+                    	$sender->sendMessage("§4[Caution] The UNBAN command is executed by the OP who banned the person, and will be rejected unless it is executed on the server that was when the person was banned.");
 		    	return true;
             	}
 		
