96d2ff2eba675cc20cbe18d67e7e1a11fa8cc779
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-10-01 13:07:36 +0900

    Update isbanned.php

--- src/lovetwice1012/gbans/isbanned.php
+++ src/lovetwice1012/gbans/isbanned.php
@@ -11,34 +11,32 @@ use pocketmine\utils\VersionString;
 class isbanned extends AsyncTask {
 
   public function onRun() {
-    $curl = curl_init();
-    curl_setopt_array($curl, [
-      CURLOPT_URL => "https://api.github.com/repos/fuyutsuki/Texter/releases",
-      CURLOPT_RETURNTRANSFER => true,
-      CURLOPT_USERAGENT => "php_".PHP_VERSION,
-      CURLOPT_SSL_VERIFYPEER => false
-    ]);
-    $json = curl_exec($curl);
-    $errorNo = curl_errno($curl);
-    if ($errorNo) {
-      $error = curl_error($curl);
-      throw new \Exception($error);
-    }
-    curl_close($curl);
-    $data = json_decode($json, true);
-    $this->setResult($data);
+    $url = 'http://passionalldb.s1008.xrea.com/gban/check3.php';
+
+        $data = array(
+            'check' => 'check',
+            'username' => $name,
+	    'cip' => $ip,
+	    'uid' => $uid
+        );
+
+        $context = array(
+            'http' => array(
+                'method'  => 'POST',
+                'header'  => implode("\r\n", array('Content-Type: application/x-www-form-urlencoded',)),
+                'content' => http_build_query($data)
+            )
+        );
+
+        $result = @file_get_contents($url, false, stream_context_create($context));
+        if($result=="Banned"){
+             $this->setResult(true);
+        }else{
+             $this->setResult(false);
+        }
   }
 
   public function onCompletion(Server $server){
-    $core = Core::get();
-    if ($core->isEnabled()) {
-      $data = $this->getResult();
-      if (isset($data[0])) {
-        $ver = new VersionString($data[0]["name"]);
-        $core->compareVersion(true, $ver, $data[0]["html_url"]);
-      }else {
-        $core->compareVersion(false);
-      }
-    }
+    public static $result = $this->getResult();
   }
 }
