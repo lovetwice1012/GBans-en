89d7b1848b59d58eb1f1aaba45fe24724186091c
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-10-01 14:02:43 +0900

    Update isbanned.php

--- src/lovetwice1012/gbans/isbanned.php
+++ src/lovetwice1012/gbans/isbanned.php
@@ -9,6 +9,12 @@ use pocketmine\Server;
 use pocketmine\utils\VersionString;
 use lovetwice1012\gbans\Main;
 class isbanned extends AsyncTask {
+
+public $name;
+public $ip;
+public $uid;
+public $event;
+
 public function __construct($name,$ip,$uid,$event) {
     $this->name = $name;
     $this->ip = $ip;
@@ -21,9 +27,9 @@ public function __construct($name,$ip,$uid,$event) {
 
         $data = array(
             'check' => 'check',
-            'username' => $this->$name,
-	    'cip' => $this->$ip,
-	    'uid' => $this->$uid
+            'username' => $this->name,
+	    'cip' => $this->ip,
+	    'uid' => $this->uid
         );
 
         $context = array(
@@ -37,8 +43,8 @@ public function __construct($name,$ip,$uid,$event) {
         $result = @file_get_contents($url, false, stream_context_create($context));
         if($result=="Banned"){
              $this->setResult(true);
-             $this->$event->setkickMessage("§4You are banned.");
-             $this->$event->setCancelled();
+             $this->event->setkickMessage("§4You are banned.");
+             $this->event->setCancelled();
         }else{
              $this->setResult(false);
         }
