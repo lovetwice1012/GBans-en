cfb3e3c360246e07d6d0ffbc4cf8438855245d59
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2020-10-01 14:03:29 +0900

    Update unban.php

--- src/lovetwice1012/gbans/unban.php
+++ src/lovetwice1012/gbans/unban.php
@@ -9,6 +9,9 @@ use pocketmine\Server;
 use pocketmine\utils\VersionString;
 use lovetwice1012\gbans\Main;
 class unban extends AsyncTask {
+public $name;
+public $sender;
+public $user;
 public function __construct($name,$user,$sender) {
     $this->name = $name;
     $this->user = $user;
@@ -20,8 +23,8 @@ public function __construct($name,$user,$sender) {
 
         $data = array(
             'unban' => 'unban',
-            'username' => $this->$name,
-            'user' => $this->$user
+            'username' => $this->name,
+            'user' => $this->user
         );
 
 
@@ -38,10 +41,10 @@ public function __construct($name,$user,$sender) {
         $result = @file_get_contents($url, false, stream_context_create($context));
         if($result=="success"){
             $this->setResult(true);
-                	           		$this->$sender->sendMessage("Canceled the damage report from this server. response: \"".$this->message."\"");
+                	           		$this->sender->sendMessage("Canceled the damage report from this server. response: \"".$this->message."\"");
         }else{
-		     	$this->$sender->sendMessage("The damage report from this server could not be undone.  The request has been rejected. response: \"".$this->message."\"");
-                    	$this->$sender->sendMessage("§4[Caution] The UNBAN command is executed by the OP who banned the person, and will be rejected unless it is executed on the server that was when the person was banned.");
+		     	$this->sender->sendMessage("The damage report from this server could not be undone.  The request has been rejected. response: \"".$this->message."\"");
+                    	$this->sender->sendMessage("§4[Caution] The UNBAN command is executed by the OP who banned the person, and will be rejected unless it is executed on the server that was when the person was banned.");
 		     $this->setResult(false);
                 	
         }
